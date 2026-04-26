2c4290c0960f55cce6d03349673df05ababff09a
LeeSF03 <shuenfeilee@gmail.com>
2026-04-26 16:54:53 +0800

    feat: remove postcss dependency

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  turbopack: {
+    rules: {
+      "*.css": {
+        loaders: ["@tailwindcss/webpack"],
+      },
+    },
+  },
   /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
+  logging: {
+    browserToTerminal: true,
+  },
+  experimental: {
+    authInterrupts: true,
+    serverComponentsHmrCache: true,
+  },
 };
 
 export default nextConfig;
