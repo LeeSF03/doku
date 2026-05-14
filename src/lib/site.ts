ad33ec4b2b489861d3e2ab793fefae457767b2a2
LeeSF <109432084+LeeSF03@users.noreply.github.com>
2026-05-14 18:57:54 +0800

    Fix metadata env var (#3)
    * chore(ui): clean up review actions and assets
    
    * chore(review): use back instead of replace when resetting draft
    
    * feat(metadata): add site metadata files
    
    * fix(metadata): wrong url env var

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,4 +1,4 @@
 export const SITE_URL =
   process.env.NODE_ENV === "production"
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL ?? process.env.VERCEL_URL}`
+    ? `https://${process.env.DEPLOY_PRIME_URL ?? process.env.DEPLOY_URL}`
     : process.env.NEXT_LOCAL_TESTING_URL || "http://localhost:3000"
